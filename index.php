466f28a84c2189ca55fd426af84beab38e1b0953
cmmeedi <89672577+cmmeedi@users.noreply.github.com>
2022-04-21 15:44:36 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,24 @@
-<php
-     
-     ?>
+<?php
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+    <title>Document</title>
+  </head>
+  <body>
+    <form
+      method="get"
+      action="https://museum-of-candy-cm.herokuapp.com/index.html"
+    >
+      <button>Click Me</button>
+    </form>
+  </body>
+</html>
